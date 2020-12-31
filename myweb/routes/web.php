16bc3a4c9ea10acd7e6dd5db9302678310a6904c
Jhen-Chen <a616032@yahoo.com.tw>
2020-12-31 19:31:04 +0800

    my first commit

--- myweb/routes/web.php
+++ myweb/routes/web.php
@@ -13,11 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/* modified by Min-Huang Ho */
 Route::get('/', function () {
     return view('welcome');
 });
-
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
